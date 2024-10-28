25daeabacf93b559540a19f93faac6fc5f4c0054
Rik <161947286+RiquelmeLadislau@users.noreply.github.com>
2024-10-28 16:55:33 -0300

    oioi

---
+++ script.js
@@ -0,0 +1,24 @@
+property = []
+let menu = ``
+let newProperty = ``
+
+do{menu = prompt(`Quantidade de imoveis ${property.lenght}
+
+    1 -Listar imoveis
+    2 -Novo imovel
+    3- Sair
+`)
+switch(menu){
+    case '2': newProperty = {
+        name: prompt('nome do dono do imovel'),
+        room: parseInt(prompt('quantos qaurtos tem?')),
+        bathroom: prompt(('quantos banheiros tem?')),
+        garage: prompt(confirm)
+        
+    }
+    newProperty.push
+    break
+    case '1'
+    alert ('')
+
+}while(menu !== '3')
